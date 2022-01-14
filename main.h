c8e217ad59afb12f436c9ede19eb5a048d7dab40
omardebouni <53655426+omardebouni@users.noreply.github.com>
2022-01-14 13:21:53 +0100

    Delete main.h

--- main.h
+++
@@ -1,7 +0,0 @@
-
-#include <stdlib.h>
-//#ifndef IMPLEMENTIERUNG_MAIN_H
-//#define IMPLEMENTIERUNG_MAIN_H
-//
-//#endif //IMPLEMENTIERUNG_MAIN_H
-double approxArsinh_series(double x, int precision);
