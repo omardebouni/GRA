27351eaecd27b072ccdd3d57faa0225206b51948
omardebouni <53655426+omardebouni@users.noreply.github.com>
2022-01-17 13:21:42 +0100

    Update custom_math.h

--- custom_math.h
+++ custom_math.h
@@ -6,7 +6,5 @@ int customFactorial(int x);
 double customAbs(double x);
 int sign(int n);
 double customLn(double x);
-void initializeLookupArray(double lookup_values[], int precision);
-double lookupValuesFormula(int n);
 double customSqrt(double x);
 double lookup_ln(double x);
