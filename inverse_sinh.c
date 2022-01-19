782354c5b082b1fb3e56c38adcfe7daa63617d37
omardebouni <53655426+omardebouni@users.noreply.github.com>
2022-01-19 13:42:02 +0100

    Update inverse_sinh.c

--- inverse_sinh.c
+++ inverse_sinh.c
@@ -1,6 +1,10 @@
 #include "custom_math.h"
+#include "math.h"
+
+double ln_table[1000];
 
 #define ITERATIONS 25
+
 
 /*Die Funktion implementiert die Formel für ein bestimmtes x mithilfe einer reinen
 Reihendarstellung und gibt das Ergebnis zurück
@@ -33,6 +37,36 @@ double approxArsinh_series(double x) {
         result = -customLn(-2 * x) + result;
     }
     return result;
+}
+
+/**
+ * This is an auxiliary function for the lookup function.
+ * It expects only values of which the natural logarithm
+ * is precalculated and saved in the lookup table array (between 1.00 and 9.99).
+ * Returns the index of the sought logarithm as an int
+ */
+int index_of(double x) {
+    return to_int((x - 1) * 100);
+}
+
+/**
+ * This is an auxiliary function for the lookup function.
+ * It returns the logarithm of the closest value to the
+ * given x parameter. The values returned are between 1.00 and 9.99
+ * with a step size of 0.01
+ */
+double get_closest_entry(double x) {
+
+}
+
+// TODO: implement ln using a lookup table
+double lookup_ln(double x){
+    if (x == 0) return -INFINITY;
+    if (x < 1) return -1 + lookup_ln(x*M_E);
+    else if (x > 10) return 1 + lookup_ln(x/M_E);
+
+    double closest_entry = get_closest_entry(x);
+    return ln_table[index_of(closest_entry)] + lookup_ln(1 + customAbs(x - closest_entry));
 }
 
 /*Die Funktion implementiert die Formel für ein bestimmtes x mithilfe eines Tabellen-
