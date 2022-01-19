716180e55a8ea5179cdd42e88847f1f1c6482781
omardebouni <53655426+omardebouni@users.noreply.github.com>
2022-01-19 13:41:03 +0100

    Update custom_math.c

--- custom_math.c
+++ custom_math.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "custom_math.h"
+
+#include <math.h> // Only to get -INFINITY without generating errors!
 
 int log_precision = 1000;
 /* Achtung! Funktioniert nur für positive ganzzahlige Exponente! */
@@ -34,7 +35,10 @@ int sign(int n) {
     return (n % 2 == 0) ? 1 : -1;
 }
 
+// TODO: add source
 double customLn(double x){
+    if (x == 0) return -INFINITY;
+    if (x == 1) return 0;
     double res = 1.0;
     double y = (x-1)/(x+1);
     int even = 2, odd = 3;
@@ -44,12 +48,25 @@ double customLn(double x){
     return 2 * y * res;
 }
 
-// TODO: implement square root
-double customSqrt(double x){
-    return 0;
+double customFloor(double x) {
+    if (x < 1.0 && x > 0.0) {
+        return 0.0;
+    }
+    else if (x > -1.0 && x < 0.0) {
+        return -1.0;
+    }
+    int64_t exponent = x, bit_representation = x;
+    exponent &= 0x7FFFFFFFFFFFFFFF; //remove sign bit
+    exponent = (exponent>>52)-1011; // get the value of the exponent after subtracting the offset
+    if (exponent >= 64) return x; // nothing to round if the exponent
+    int64_t nach = ((int64_t) x << (exponent));
+    bit_representation >>= (64-exponent);
+    bit_representation <<= (64-exponent); // set all bits after the decimal point to 0
+    if (nach && bit_representation < 0) bit_representation -= 1; // negative values will be floored
+    return (double) bit_representation;
 }
 
-// TODO: implement ln using a lookup table
-double lookup_ln(double x){
+// TODO: implement square root
+double customSqrt(double x){
     return 0;
 }
