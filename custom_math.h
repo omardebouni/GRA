d8e6d047b3ce9987f3db9f1d277b7468158a327a
omardebouni <53655426+omardebouni@users.noreply.github.com>
2022-01-19 13:41:20 +0100

    Update custom_math.h

--- custom_math.h
+++ custom_math.h
@@ -5,6 +5,6 @@ double customPow(double basis, int exponent);
 int customFactorial(int x);
 double customAbs(double x);
 int sign(int n);
+double customFloor(double x);
 double customLn(double x);
 double customSqrt(double x);
-double lookup_ln(double x);
