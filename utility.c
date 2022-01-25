17aca5997168a80ac0e34aeb1a4ffcffa1619188
omardebouni <53655426+omardebouni@users.noreply.github.com>
2022-01-25 12:15:52 +0100

    Update utility.c

--- utility.c
+++ utility.c
@@ -232,7 +232,7 @@ void run_test() {
             //if (epsilon == 0) predicate = (got != expected);
             //else predicate = customAbs(got - expected) >= epsilon;
             if (got != expected) { 
-                failed++;
+                printf("failed: %d\n",++failed);
                 if (print) printf("asinh(%lf):\n\tGot:      %lf\n\tExpected: %lf\n", i, got, expected);
             }
         }
